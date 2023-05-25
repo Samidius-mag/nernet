0e2fb73cb9e756816c4b083a2327c044084849a8
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-25 21:44:12 +1000

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-const tf = require('@tensorflow/tfjs');
+/*const tf = require('@tensorflow/tfjs');
 const fs = require('fs');
 
 const data = JSON.parse(fs.readFileSync('price.json'));
@@ -137,3 +137,90 @@ function getPrediction(data, interval) {
   const prediction = model.predict(xs);
   return prediction;
 }
+*/
+const tf = require('@tensorflow/tfjs');
+const fs = require('fs');
+
+// Загрузка данных из файла price.json
+const rawData = fs.readFileSync('price.json');
+const data = JSON.parse(rawData);
+
+// Преобразование данных в тензоры
+const inputTensor = tf.tensor(data.map(candle => [
+  candle.open,
+  candle.high,
+  candle.low,
+  candle.close,
+  candle.volume,
+]));
+const outputTensor = tf.tensor(data.map(candle => [
+  candle.close,
+]));
+
+// Создание модели нейросети
+const model = tf.sequential();
+model.add(tf.layers.dense({ units: 64, inputShape: [5], activation: 'relu' }));
+model.add(tf.layers.dense({ units: 1, activation: 'linear' }));
+model.compile({ optimizer: 'adam', loss: 'meanSquaredError' });
+
+// Обучение модели на данных
+model.fit(inputTensor, outputTensor, { epochs: 54 })
+  .then(() => {
+    // Получение предсказаний для текущего часа
+    const currentCandle = data[data.length - 1];
+    const currentInput = tf.tensor([[
+      currentCandle.open,
+      currentCandle.high,
+      currentCandle.low,
+      currentCandle.close,
+      currentCandle.volume,
+    ]]);
+    const currentPrediction = model.predict(currentInput).dataSync()[0];
+
+    // Получение предсказаний для следующих 1, 4, 12 и 24 часов
+    const nextPredictions = [];
+    for (let i = 1; i <= 24; i++) {
+      const nextCandle = data[data.length - 1 + i];
+      const nextInput = tf.tensor([[
+        nextCandle.open,
+        nextCandle.high,
+        nextCandle.low,
+        nextCandle.close,
+        nextCandle.volume,
+      ]]);
+      const nextPrediction = model.predict(nextInput).dataSync()[0];
+      nextPredictions.push(nextPrediction);
+    }
+
+    // Определение движения тренда относительно всех данных, данных 24, 12, 4 и текущего часа
+    const trend = {
+      all: currentPrediction > data[0].close ? 'up' : 'down',
+      24: currentPrediction > data[data.length - 25].close ? 'up' : 'down',
+      12: currentPrediction > data[data.length - 13].close ? 'up' : 'down',
+      4: currentPrediction > data[data.length - 5].close ? 'up' : 'down',
+      current: currentPrediction > currentCandle.close ? 'up' : 'down',
+    };
+
+    // Определение уровней поддержки и сопротивления для текущего направления движения
+    const support = trend.current === 'up' ? currentCandle.low : currentCandle.high;
+    const resistance = trend.current === 'up' ? currentCandle.high : currentCandle.low;
+
+    // Определение уровней отскоков цен для текущего направления движения
+    const bounce1 = trend.current === 'up' ? currentCandle.close * 0.98 : currentCandle.close * 1.02;
+    const bounce2 = trend.current === 'up' ? currentCandle.close * 0.96 : currentCandle.close * 1.04;
+
+    // Создание предсказания будущего движения для последующих 1, 4, 12, 24 часов с ценами
+    const predictions = nextPredictions.map((prediction, index) => ({
+      hours: (index + 1),
+      price: prediction,
+    }));
+
+    console.log('Trend:', trend);
+    console.log('Support:', support);
+    console.log('Resistance:', resistance);
+    console.log('Bounce levels:', bounce1, bounce2);
+    console.log('Predictions:', predictions);
+  })
+  .catch(error => {
+    console.error(error);
+  });
