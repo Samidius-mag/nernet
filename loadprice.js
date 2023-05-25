9d58a75f11404248e9ccab7c1f1068667f6b0536
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-25 18:38:32 +1000

    Update loadprice.js

--- loadprice.js
+++ loadprice.js
@@ -1,25 +1,11 @@
 const axios = require('axios');
 const fs = require('fs');
 
-const url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=5000';
-
-axios.get(url)
+axios.get('https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=5000')
   .then(response => {
-    const data = response.data.map(candle => ({
-      time: candle[0],
-      open: parseFloat(candle[1]),
-      high: parseFloat(candle[2]),
-      low: parseFloat(candle[3]),
-      close: parseFloat(candle[4]),
-      volume: parseFloat(candle[5]),
-      closeTime: candle[6],
-      quoteAssetVolume: parseFloat(candle[7]),
-      numberOfTrades: candle[8],
-      takerBuyBaseAssetVolume: parseFloat(candle[9]),
-      takerBuyQuoteAssetVolume: parseFloat(candle[10])
-    }));
+    const data = response.data;
     fs.writeFileSync('price.json', JSON.stringify(data));
-    console.log('Data saved to price.json');
+    console.log('Записано в файл price.json');
   })
   .catch(error => {
     console.log(error);
