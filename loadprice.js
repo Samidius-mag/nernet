9a5e32fd34025adc40ea4e79f46adc9b44a4fc50
Samidius (VoroniN) <78709877+Samidius-mag@users.noreply.github.com>
2023-05-25 18:11:59 +1000

    Create loadprice.js

---
+++ loadprice.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const fs = require('fs');
+
+const url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=5000';
+
+axios.get(url)
+  .then(response => {
+    const data = response.data.map(candle => ({
+      time: candle[0],
+      open: parseFloat(candle[1]),
+      high: parseFloat(candle[2]),
+      low: parseFloat(candle[3]),
+      close: parseFloat(candle[4]),
+      volume: parseFloat(candle[5]),
+      closeTime: candle[6],
+      quoteAssetVolume: parseFloat(candle[7]),
+      numberOfTrades: candle[8],
+      takerBuyBaseAssetVolume: parseFloat(candle[9]),
+      takerBuyQuoteAssetVolume: parseFloat(candle[10])
+    }));
+    fs.writeFileSync('price.json', JSON.stringify(data));
+    console.log('Data saved to price.json');
+  })
+  .catch(error => {
+    console.log(error);
+  });
